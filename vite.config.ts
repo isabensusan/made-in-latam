339f9947707bf80fecc7238bfc92e97ca71a307d
isabensusan <isabensusan@gmail.com>
2025-03-19 17:02:49 -0300

    updated gh pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: process.env.NODE_ENV === 'production' ? '/made-in-latam/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   server: {
     host: "::",
     port: 8080,
