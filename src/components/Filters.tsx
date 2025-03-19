1fe12b458685ec47350e30097f5e1a5e74799c49
isabensusan <isabensusan@gmail.com>
2025-03-19 14:57:12 -0300

    made filters scrollable

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -1,6 +1,7 @@
 
 import { Button } from "@/components/ui/button";
 import { FilterType } from "@/types/startup";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area"
 
 interface FiltersProps {
   techVertical: string[];
@@ -19,50 +20,54 @@ export const Filters = ({
     <div className="mb-8">
       <div className="mb-4">
         <h3 className="mb-2 text-sm font-medium text-gray-500">Categories</h3>
-        <div className="flex flex-wrap gap-2">
-          <Button
-            variant={!activeFilters.techVertical ? "default" : "outline"}
-            size="sm"
-            onClick={() => onFilterChange("techVertical", null)}
-          >
-            All
-          </Button>
-          {techVertical.map((techVertical) => (
+        <ScrollArea className="w-full whitespace-nowrap rounded-md">
+          <div className="flex flex-wrap gap-2 w-max space-x-1">
             <Button
-              key={techVertical}
-              variant={activeFilters.techVertical === techVertical ? "default" : "outline"}
+              variant={!activeFilters.techVertical ? "default" : "outline"}
               size="sm"
-              onClick={() => onFilterChange("techVertical", techVertical)}
+              onClick={() => onFilterChange("techVertical", null)}
             >
-              {techVertical}
+              All
             </Button>
-          ))}
-        </div>
+            {techVertical.map((techVertical) => (
+              <Button
+                key={techVertical}
+                variant={activeFilters.techVertical === techVertical ? "default" : "outline"}
+                size="sm"
+                onClick={() => onFilterChange("techVertical", techVertical)}
+              >
+                {techVertical}
+              </Button>
+            ))}
+          </div>
+          <ScrollBar orientation="horizontal" className="hidden" />
+        </ScrollArea>
       </div>
       
       <div>
         <h3 className="mb-2 text-sm font-medium text-gray-500">Country</h3>
-
-        <div className="flex flex-wrap gap-2">
-          <Button
-            variant={!activeFilters.country ? "default" : "outline"}
-            size="sm"
-            onClick={() => onFilterChange("country", null)}
-          >
-            All
-          </Button>
-          {country.map((country) => (
+        <ScrollArea className="w-full whitespace-nowrap rounded-md">
+          <div className="flex flex-wrap gap-2 w-max space-x-1">
             <Button
-              key={country}
-              variant={activeFilters.country === country ? "default" : "outline"}
+              variant={!activeFilters.country ? "default" : "outline"}
               size="sm"
-              onClick={() => onFilterChange("country", country)}
+              onClick={() => onFilterChange("country", null)}
             >
-              {country}
+              All
             </Button>
-          ))}
-        </div>
-
+            {country.map((country) => (
+              <Button
+                key={country}
+                variant={activeFilters.country === country ? "default" : "outline"}
+                size="sm"
+                onClick={() => onFilterChange("country", country)}
+              >
+                {country}
+              </Button>
+            ))}
+          </div>
+          <ScrollBar orientation="horizontal" className="hidden" />
+        </ScrollArea>
       </div>
     </div>
   );
